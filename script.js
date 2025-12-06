ac7f881ac6439487d7b3d35d13572b025415b061
Millind amb <millindamb4@gmail.com>
2025-12-06 12:32:03 +0530

    About Promises

--- script.js
+++ script.js
@@ -1,98 +1,122 @@
-//async javascript fundamental
+//API Call using XMLhttpsRequest(the old method before fetch)
+const URL=`https://api.github.com/users/millindamb`
 
-//as default javascript is a synchronous and single threaded language 
+const xhr=new XMLHttpRequest()//making the object of the XMLHttpRequest class to use it
 
-// there two types of code: Blocking Code and Non Blocking Code 
-//blocking code: block the flow of the program
-//non blocking code: Does not block execution
+xhr.open('GET',URL)//defining the use and the url of the api(this will not track the status continously)
 
-// console.log("First")
-// setTimeout(() => {
-//     console.log("Hello World")
-// }, 0);//even if the time is 0,these line will run after printing second
-// //as this setime out will work as a call ans in that time the console get run
-// console.log("Second")
+xhr.onreadystatechange=function(){//this will continously track if the status of the api call change and perform the work
 
-// //set time out will do the work after a time once
-// setTimeout(() => {
-//     console.log("Example of setTimeout")
-// }, 2000);
-// //but setInterval will do the work again and again after that time
-// setInterval(()=>{
-//     console.log("Example of setInterval")
-// },5000)
+    // console.log(xhr.readyState)//printing the status of the api call
 
-//how to stop a setTimeout using the clearTimeout function
-// const changeMe=setTimeout(()=>{console.log("hello")},2000)
-// clearTimeout(changeMe)//this will stop the changeMe 
-
-//setTimeout
-const changeHeading=setTimeout(()=>{
-    document.querySelector(".heading").innerHTML="Something is Wrong"
-},5000)
-
-const btn=document.querySelector(".button")
-btn.addEventListener("click",()=>{
-    clearTimeout(changeHeading)
-    console.log("Changes Stoped")
-})
-
-//setInterval
-
-const sayDate=(name)=>{
-    console.log("hello",name,"the Time is :",Date.now().toString())
-}
-let interval
-const stBtn=document.querySelector("#start")
-const spBtn=document.querySelector("#stop")
-
-spBtn.addEventListener("click",()=>{clearInterval(interval)})
-stBtn.addEventListener("click",()=>{interval=setInterval(sayDate,5000,"User")})
-
-//Project: Color Changer
-//generate a random color in hex code
-
-const randomColor=()=>{
-    const hex="0123456789ABCDEF"
-    let color="#"
-    for(let i=0;i<6;i++){
-        const idx=Math.floor(Math.random()*16)
-        color+=hex[idx]
+    if(xhr.readyState===4){
+        // const profileInfo=JSON.parse(this.responseText)//convertint the string data into object
+        // console.log(profileInfo.followers)
     }
-    return color
 }
-let colorChanger
-const spColor=document.querySelector("#spColor")
-const stColor=document.querySelector("#stColor")
-const bg=document.querySelector("body")
-spColor.addEventListener("click",()=>{
-    clearInterval(colorChanger)
-    colorChanger=null;//as there is no use of the colorChanger (for optimization) dereference
-})
-stColor.addEventListener("click",()=>{
-    if(!colorChanger){
-        colorChanger=setInterval(()=>{
-            const color=randomColor()
-            bg.style.backgroundColor=color
-        },2500)
-    }
+
+xhr.send()//sending the request
+
+//About Console
+//console is a debugger tool and this is injected during the runtime
+//V8 engine provides all the debugger tool 
+
+//Promise 
+//promises are either consume or created
+//mainly promise are consume, Example:
+
+//first way
+const promiseOne=new Promise((resolve,reject)=>{//here the arrow function is a call back
+    setTimeout(() => {
+        // console.log("task is Complete")
+        // resolve()
+        // reject()
+    }, 2000)
 })
 
-//project 2 key tracker
-const insert=document.querySelector("#insert")
-window.addEventListener('keydown',(e)=>{
-    insert.innerHTML=`
-    <table>
-        <tr>
-            <th>key</th>
-            <th>keyCode</th>
-            <th>code</th>
-        </tr>
-        <tr>
-            <td>${e.key}</td>
-            <td>${e.keyCode}</td>
-            <td>${e.code}</td>
-        </tr>
-    </table>
-    `
+promiseOne.then(()=>{//this function will work if the promise gets resolved
+    // console.log("promise is resolved")
 })
+
+promiseOne.catch(()=>{//this will work when the promise get rejected
+    //console.log("promise is rejected")//and will give a error
+})
+
+//second way
+new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        // console.log("2nd task is Complete")
+        resolve()
+    }, 2000)
+})//.then(()=>{console.log("2nd promise is completed")}).catch(()=>{console.log("2nd promise got rejected")})
+
+//third way: passing parameter from promise to then by using the resolve 
+const promiseThird=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve({userName:"Something",age:45})
+    },1000)
+}).then((userInfo)=>{//here userInfo is a object passed by the resolve
+    // console.log(`user name is :${userInfo.userName} and age of the user is :${userInfo.age}`)
+})
+
+//fourth way
+const promiseFour=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error=true
+        if(!error){
+            resolve({userName:"Something",age:45})
+        }else{
+            reject("Something went wrong")
+        }
+    },5000)
+})
+.then((user)=>{
+    return user.userName
+})
+.then((username)=>{
+    console.log(username);
+})
+.catch((errorMsg)=>{
+    // console.log(errorMsg)
+})
+
+//fifth way: using async await
+const promiseFive=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error=true
+        if(error){
+            resolve({userName:"js",password:4545})
+        }else{
+            reject("ERROR: Something went wrong")
+        }
+    },1000)
+})
+async function fifth(){
+    try{
+        const response=await promiseFive
+        console.log(response)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+fifth()
+
+//fetching a api using the async await (1st way)
+
+// async function afunction(){
+//     try{
+//         let response=await fetch(URL)
+//         const data=await response.json()
+//         console.log(data)
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
+
+//fetching a api using then and catch (2nd way)
+fetch(URL)
+.then((response)=>{return response.json()})
+.then((data)=>{console.log(data)})
+.catch((error)=>{console.log(`E: ${error}`)})
+// afunction()
